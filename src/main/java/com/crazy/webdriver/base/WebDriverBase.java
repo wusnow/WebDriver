c59d5ffe87f0f6ef829b1ad4732ee4a8f24bd963
Administrator <Administrator@KYE-1000415093>
2018-09-04 09:55:11 +0800

    fixed

--- src/main/java/com/crazy/webdriver/base/WebDriverBase.java
+++ src/main/java/com/crazy/webdriver/base/WebDriverBase.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
